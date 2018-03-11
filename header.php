b7c29ce63e8bb04b737bfc41a193de5ef050ad0c
Ron Leezer <rontlum@gmail.com>
2018-03-10 14:24:31 -1000

    Making those base files
    - added this 960 grid (not responsive, not bootsrap, but flat width)
    - added some structure with folders
    - added our dynamic footer and header files, which repeat on each page

---
+++ header.php
@@ -0,0 +1,40 @@
+<!--
+Project Name: Lei Ana Decision Support System (temporary name)
+Group: Chin Cameron / Lum Ron / Mateo Blake / Shrestha Niju / Wurzel Alex
+Developers: Came-Ron
+Repository: https://github.com/ronlum/Lei-Ana-DSS
+PM Page: https://trello.com/b/S5Q571K7/is6120-software-practicum (private)
+Structure: not responsive, 960px grid
+-->
+<!DOCTYPE> 
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--below is to tell robots to go away and request not index-->
+<meta name="robots" content="noindex, nofollow">
+<!-- dynamic title below -->
+<title><?php echo $title . " | ". $sitename; ?></title> 
+<!-- general, user customized stylesheet-->
+<link rel="stylesheet" href="css/style.css">
+<!-- stylesheet for our grid system 960px -->
+<link rel="stylesheet" href="css/960_12_col.css">
+<script src="js/jquery-3.2.1.min.js"></script>
+<!-- this will be the primary place to make scripts to keep our <head> clean-->
+<script src="js/script.js"></script> 
+<!--Load the GOOGLE CHARTS API-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</head>
+<body>
+
+<!-- MENU-->
+<header id="header">
+<nav id="nav">
+	<ul>
+		<li><a href="#">Menu Item</a></li>
+		<li><a href="#">Menu Item</a></li>
+		<li><a href="#">Menu Item</a></li>
+		<li><a href="#">Menu Item</a></li>
+	</ul>
+</nav>
+</header> 
