9cf8364aeac3fbcd52f4b28f4e36b35af4535c3c
CamAero <CamAero@users.noreply.github.com>
2018-03-12 21:52:07 -1000

    Add Degree plan.  Move schedule to root.

---
+++ Degree.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php
+//Add navbar and crud delete
+include_once './check_session.php';
+include_once './navbar.html';
+include_once './crud/delete.php';
+?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Degree Plan</title>
+
+  </head>
+  <body>
+
+  </body>
+</html>
