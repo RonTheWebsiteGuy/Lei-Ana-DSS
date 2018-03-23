126280a884b547b91d82701951440728dd67512b
CamAero <CamAero@users.noreply.github.com>
2018-03-22 17:50:58 -1000

    Add view

--- Degree.php
+++ Degree.php
@@ -1,4 +1,21 @@
 <?php
 require_once 'dbconnect.php'
  ?>
- 
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Degree</title>
+  </head>
+  <body>
+    <?php 
+    ?>
+  </body>
+</html>
+
+//SQL Statement to get all majors with classes requirements
+SELECT Majors.mName, Classes.Class_Name, Classes.class_Number
+FROM Majors
+Inner Join Prerequisites ON Majors.majorID=Prerequisites.MajorID
+Inner Join Classes ON Prerequisites.ClassID=Classes.classID;
