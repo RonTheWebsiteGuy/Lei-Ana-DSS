1e853cbef61c6370f752815fbec699701a58ca82
CamAero <CamAero@users.noreply.github.com>
2018-03-22 18:45:18 -1000

    Fix the connection for the db

--- includes/conn.php
+++ includes/conn.php
@@ -1,7 +1,7 @@
 <?php 
 
 $host = 'is6120db.cg2tsjxicqqb.us-east-1.rds.amazonaws.com';
-$dbname = 'is6120db'; 
+$dbname = 'Degree_Plan'; 
 $port = 3306;
 $user = 'ronster';
 $pass = 'tidepodmarine';
@@ -10,7 +10,7 @@ $pass = 'tidepodmarine';
 //PDO here
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $conn = new PDO("mysql:host=$host;port=$port; dbname=$dbname", $user, $pass);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
