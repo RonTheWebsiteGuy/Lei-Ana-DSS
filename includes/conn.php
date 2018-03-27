1ad31a9570f33203c1cd96cccd7ea71bbbc95e6a
CamAero <CamAero@users.noreply.github.com>
2018-03-26 19:35:26 -1000

    Comment out the errors
    What is the "<<<HEAD" for?

--- includes/conn.php
+++ includes/conn.php
@@ -1,24 +1,24 @@
-<?php 
+<?php
 //set to exception mode
-$dbname = 'Degree_Plan'; 
+$dbname = 'Degree_Plan';
 $host = 'is6120db.cg2tsjxicqqb.us-east-1.rds.amazonaws.com';
-<<<<<<< HEAD
-$dbname = 'Degree_Plan'; 
-=======
->>>>>>> 8be32d3035e9f0324dfeb635d72a6cccd56151ac
+//<<<<<<< HEAD
+$dbname = 'Degree_Plan';
+//=======
+//>>>>>>> 8be32d3035e9f0324dfeb635d72a6cccd56151ac
 $port = 3306;
 $user = 'ronster';
 $pass = 'tidepodmarine';
 
 
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 try {
     $conn = new PDO("mysql:host=$host;port=$port; dbname=$dbname", $user, $pass);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-=======
+//=======
 
->>>>>>> 8be32d3035e9f0324dfeb635d72a6cccd56151ac
+//>>>>>>> 8be32d3035e9f0324dfeb635d72a6cccd56151ac
 ?>
