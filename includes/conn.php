72c94cbd9f498bc521d94863e961dc715cb5253e
Macbook Ron <rontlum@gmail.com>
2018-03-26 16:09:19 -1000

    push for cameron to see

--- includes/conn.php
+++ includes/conn.php
@@ -1,17 +1,12 @@
 <?php 
-
+//set to exception mode
+$dbname = 'Degree_Plan'; 
 $host = 'is6120db.cg2tsjxicqqb.us-east-1.rds.amazonaws.com';
-$dbname = 'is6120db'; 
 $port = 3306;
 $user = 'ronster';
 $pass = 'tidepodmarine';
 
 
-//PDO here
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+
 ?>
---
+++ program-requirements.php
@@ -0,0 +1,81 @@
+<?php include('includes/conn.php'); ?>
+<?php $title = "Degrees and Programs"; include('includes/header.php'); ?>
+
+<script>
+jQuery(document).ready( function () {
+    jQuery('#programs').DataTable();
+} );
+</script>
+
+<h1><?php echo $title; ?></h1>
+<!--
+<table id="programs">
+<thead>
+<tr><th>Program Name</th><th>Classification</th><th>Enrollment</th></tr>
+</thead>
+<tbody>
+<tr><td>MSIS</td><td>Graduate</td><td>25</td></tr>
+<tr><td>MBA</td><td>Graduate</td><td>50</td></tr>
+<tr><td>BBA</td><td>Undergraduate</td><td>500</td></tr>
+</tbody>
+
+
+</table>
+-->
+<?php 
+
+	
+
+try {
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+$sth = $conn->prepare('SELECT * FROM `Prerequisites`');
+
+$sth->execute();
+
+?>
+
+<table id="programs">
+<tr><th>Prereq ID</th><th>Class ID</th><th>Major Id</th><th>Note</th></tr>
+<?php 
+foreach ($sth as $item) {
+	echo "<tr>";
+	echo "<td>";
+	echo $item[0];
+	echo "</td><td>";
+	echo $item[1];
+	echo "</td><td>";
+	echo $item[2];
+	echo "</td><td>";
+	echo $item[3];
+	echo "</td>";
+}
+
+
+
+
+} catch(PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+
+
+
+$conn = NULL; // kill connection data
+
+?>
+
+
+
+
+
+
+<?php include('includes/footer.php'); ?>
