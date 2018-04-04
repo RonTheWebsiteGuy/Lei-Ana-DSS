d67aabd469ed5aa5d1f73523811e5dd6bac37355
CamAero <CamAero@users.noreply.github.com>
2018-04-03 21:36:07 -1000

    Edit scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,9 @@
 $(document).ready(function() {
-
 	$('.edit-program').click(function() {
-		//var x = $(this).closest("tr").find(".programid").text();
-		//$(this).closest("tr").find(".programid").prepend('<input type="text" value="'+ x +'"><br>');
-
-		var y = $(this).closest("tr").find(".programname").text();
-		$(this).closest("tr").find(".programname").prepend('<input type="text" value="' + y + '"><br>');
-
-		$(this).replaceWith('<button class="save-program">Save</button>');
-
-	});
-});
-
-
-
-//bind to element not in existence
-$('body').on('click','.save-program',function() {
-	$.ajax({
-    	data: {
-    		'MajorID' : $(this).closest("tr").find(".programid").text(),
-    		'Mname' : $(this).closest("tr").find(".programname").text()
-    	},
-    	type: "post",
-
-    	url: "../processing/save-program.php",
     	success: function(data){
-        	alert("Saved Program: " + data);
-        }
-	});
-});
-
-=======
 /*
 Table of Contents
-1. CRUD
+1. Edit and Save buttons
 	1.1 STUDENT
 	1.2 Program
 	1.3 Course
@@ -41,44 +11,25 @@ Table of Contents
 
 */
 
-///~~~~~~~~~~~~~~~~~~~~~ students-master.php ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 $(document).ready(function() {
+
+
+
+	//1.1
 	/////////////////////////////////EDIT STUDENT/////////////////////////
 	$('.edit-student').click(function() {
 			//get original data
 			var x = $(this).closest("tr").find(".lname").text();
 			var y = $(this).closest("tr").find(".fname").text();
-			var z = $(this).closest("tr").find(".majorid span").text();
-
-			//add inputs, with original data values
+			//add inputs
 			$(this).closest("tr").find(".lname").append('<input type="text" value="' + x +'">');
 			$(this).closest("tr").find(".fname").append('<input type="text" value="' + y +'">');
-			$(this).closest("tr").find(".majorid select").val(z);
-			$(this).closest("tr").find(".majorid select").removeClass("hideit");
 
-
-			//show and hide buttons
-			$(this).parent().children(".edit-student, .remove-student").addClass("hideit");
+			$(this).parent().children().addClass("hideit");
 			$(this).parent().children(".save-student, .cancel-student").removeClass("hideit");
 	});
 
-		////////////////////// CANCEL STUDENT////////////////////////////////
-		$('.cancel-student').click(function() {
-			//hide inputs
-			$(this).closest("tr").find("input").hide();
-			$(this).closest("tr").find("select").addClass("hideit");
-
-			//show and hide buttons
-			$(this).parent().children(".edit-student, .remove-student").removeClass("hideit");
-			$(this).parent().children(".save-student, .cancel-student").addClass("hideit");
-	});
-
-
-
-
-
-	////////////////////////////////SAVE BUTTON///////////////////////////////
+  ////////////////////////////////SAVE STUDENT BUTTON///////////////////////////////
 	$('.save-student').click(function() {
 		//	ajax post function
 		$.ajax({
@@ -88,8 +39,7 @@ $(document).ready(function() {
 				//locate data fields
 				sid : $(this).closest("tr").find(".sid").text(),
 				lname : $(this).closest("tr").find(".lname input").val(),
-				fname : $(this).closest("tr").find(".fname input").val(),
-				majorid : $(this).closest("tr").find(".majorid select").val()
+				fname : $(this).closest("tr").find(".fname input").val()
 			},
 			success:function(data) {
 				alert("successful");
@@ -106,48 +56,11 @@ $(document).ready(function() {
 	//do this after...search and replace, remove and add back buttons
 	var x = $(this).closest("tr").find(".lname input").val();
 	var y = $(this).closest("tr").find(".fname input").val();
-	var z = $(this).closest("tr").find(".majorid select").val();
 	$(this).closest("tr").find(".lname").text(x);
 	$(this).closest("tr").find(".fname").text(y);
-	$(this).closest("tr").find(".majorid span").text(z);
-
-	$(this).closest("tr").find(".majorid select").addClass("hideit");
-
 
 	$(this).parent().children(".save-student, .cancel-student").addClass("hideit");
 	$(this).parent().children(".edit-student, .remove-student").removeClass("hideit");
-
-	});
-
-
-
-
-
-
-	$('#add-student').click(function() {
-		//	ajax post function
-		$.ajax({
-			url: 'processing/add-student.php',
-			type: "POST",
-			data: {
-				//locate data fields
-				lname : $("#addfname").val(),
-				fname : $("#addlname").val(),
-				majorid : $("#addmajorid").val()
-			},
-			success:function(data) {
-				alert(data);
-
-
-
-			},
-			error: function() {
-				alert("error ajax");
-			}
-
-
-      });
-
 
 	});
 
@@ -205,10 +118,10 @@ $(document).ready(function() {
 	$('.edit-course').click(function() {
 			//get original data
 			var x = $(this).closest("tr").find(".cname").text();
-			var y = $(this).closest("tr").find(".crn").text();
+			//var y = $(this).closest("tr").find(".crn").text();
 			//add inputs
 			$(this).closest("tr").find(".cname").append('<input type="text" value="' + x +'">');
-			$(this).closest("tr").find(".crn").append('<input type="text" value="' + y +'">');
+		//	$(this).closest("tr").find(".crn").append('<input type="text" value="' + y +'">');
 
 			$(this).parent().children().addClass("hideit");
 			$(this).parent().children(".save-course, .cancel-course").removeClass("hideit");
@@ -224,7 +137,7 @@ $(document).ready(function() {
 				//locate data fields
 				cid : $(this).closest("tr").find(".cid").text(),
 				cname : $(this).closest("tr").find(".cname input").val(),
-				crn : $(this).closest("tr").find(".crn input").val()
+				//crn : $(this).closest("tr").find(".crn input").val()
 			},
 			success:function(data) {
 				alert("successful");
@@ -240,9 +153,9 @@ $(document).ready(function() {
 			});
 	//do this after...search and replace, remove and add back buttons
 	var x = $(this).closest("tr").find(".course input").val();
-	var y = $(this).closest("tr").find(".crn input").val();
+//	var y = $(this).closest("tr").find(".crn input").val();
 	$(this).closest("tr").find(".course").text(x);
-	$(this).closest("tr").find(".crn").text(y);
+	//$(this).closest("tr").find(".crn").text(y);
 
 	$(this).parent().children(".save-course, .cancel-course").addClass("hideit");
 	$(this).parent().children(".edit-course, .remove-course").removeClass("hideit");
@@ -253,15 +166,11 @@ $(document).ready(function() {
 	/////////////////////////////////EDIT Individual Course/////////////////////////
 	$('.edit-icourse').click(function() {
 			//get original data
-			var w = $(this).closest("tr").find(".cname").text();
-			var x = $(this).closest("tr").find(".cnumber").text();
-			var y = $(this).closest("tr").find(".pid").text();
-			var z = $(this).closest("tr").find(".crn").text();
+			var x = $(this).closest("tr").find(".cname").text();
+			var y = $(this).closest("tr").find(".crn").text();
 			//add inputs
-			$(this).closest("tr").find(".cname").append('<input type="text" value="' + w +'">');
-			$(this).closest("tr").find(".cnumber").append('<input type="text" value="' + x +'">');
-			$(this).closest("tr").find(".pid").append('<input type="text" value="' + y +'">');
-			$(this).closest("tr").find(".crn").append('<input type="text" value="' + z +'">');
+			$(this).closest("tr").find(".cname").append('<input type="text" value="' + x +'">');
+			$(this).closest("tr").find(".crn").append('<input type="text" value="' + y +'">');
 
 			$(this).parent().children().addClass("hideit");
 			$(this).parent().children(".save-icourse, .cancel-icourse").removeClass("hideit");
@@ -294,19 +203,22 @@ $(document).ready(function() {
 
 			});
 	//do this after...search and replace, remove and add back buttons
-	var w = $(this).closest("tr").find(".cname input").val();
-	var x = $(this).closest("tr").find(".cnumber input").val();
-	var y = $(this).closest("tr").find(".pid input").val();
-	var z = $(this).closest("tr").find(".crn input").val();
-	$(this).closest("tr").find(".cname").text(w);
-	$(this).closest("tr").find(".cnumber").text(x);
-	$(this).closest("tr").find(".pid").text(y);
-	$(this).closest("tr").find(".crn").text(z);
+	var x = $(this).closest("tr").find(".course input").val();
+	var y = $(this).closest("tr").find(".crn input").val();
+	$(this).closest("tr").find(".course").text(x);
+	$(this).closest("tr").find(".crn").text(y);
 
 	$(this).parent().children(".save-icourse, .cancel-icourse").addClass("hideit");
 	$(this).parent().children(".edit-icourse, .remove-icourse").removeClass("hideit");
 
 	});
+
+
+
+
+
+
+
 
 
 
